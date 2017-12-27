8cf8f6702e301b563fb8b80295d17a266c13fca8
Marterminator <martin.quanz@gmail.com>
2017-12-27 10:54:44 +0100

    inserted main function

--- src/crawly.py
+++ src/crawly.py
@@ -1,28 +1,30 @@
 import os
 
-#note that there only have to be pictures and folders in your directory
-
-#note that backslashes in path has to be doubled
+#note that backslashes in path have to be doubled
 PATH = 'C:\\Users\\Ines\\Documents\\MartinDokumente\\projects\\Pictures'
 
-#change working directory to PATH
-os.chdir(PATH)
-print(os.getcwd())
+def main():
+    #change working directory to PATH
+    os.chdir(PATH)
+    print(os.getcwd())
 
-#get elements, what is folder & what is picture?
-def getelements():
-    elements = (os.listdir())
-    pictures = []
-    folders = []
-    for e in elements:
-        try:
-            os.chdir(PATH + '\\' + e)
-            folders.append(e)
-        except NotADirectoryError:
-            pictures.append(e)
-    return pictures, folders
+    #get elements, what is folder & what is picture?
+    def getelements():
+        elements = (os.listdir())
+        pictures = []
+        folders = []
+        for e in elements:
+            try:
+                os.chdir(PATH + '\\' + e)
+                folders.append(e)
+            except NotADirectoryError:
+                pictures.append(e)
+        return pictures, folders
 
-print(getelements())
+    print(getelements())
 
 #change last element name
 #os.rename(elements[-1], 'Zehleabend2016')
+
+if __name__ == '__main__':
+    main()
